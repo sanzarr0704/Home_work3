2eaf1e2e967e5de1d136a8b46cb9f6515d122b87
Sanjar Saparov <117997805+sanzarr0704@users.noreply.github.com>
2022-11-12 11:13:26 +0600

    Это д/з 3

---
+++ DZ3_sanjar_20-01.js
@@ -0,0 +1,18 @@
+function GGG(A) {
+    if (A > 0) {
+        console.log("!")
+    } else {
+        console.log("!!")
+    }
+}
+var i = "x";
+while (i <= 10) {
+    alert(i);
+ i++;
+}
+for (var i = "x"; i <= 10; i++) {
+    alert(i);
+}
+for ( let i = 1; i <= 4; i++) {
+    console.log(i + " funk");
+}
